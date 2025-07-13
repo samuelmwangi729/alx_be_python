996b824ac50b6b3da2e8a96cf0ad5693dffce3c0
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2025-07-13 11:04:50 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
 #!usr/bin/python
-number1=10
-number2=5
+number1 = 10
+number2 = 5
 
 print(f"Addition of {number1} and {number2} is {number2+number1}")
 print(f"Subtraction of {number1} and {number2} is {number1-number2}")
